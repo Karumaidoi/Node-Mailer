cb18a8669c7e856d69648db8dbb56c78f77ccd6c
Karumaidoi <mainahmwangi12@gmail.com>
2023-09-15 13:15:18 +0300

    Solved BUG and added dotenv configurations

--- app.js
+++ app.js
@@ -2,34 +2,33 @@ const nodemailer = require("nodemailer");
 const dotenv = require("dotenv").config();
 
 console.log(process.env.EMAIL_ADDRESS);
-// async function sendEmailUser() {
-//   console.log("Sending mail");
-//   const mailTransporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 465,
-//     secure: true,
-//     auth: {
-//       user: "mainahmwangi12@gmail.com",
-//       pass: "mdyrwqswwolzpmjm",
-//     },
-//   });
+async function sendEmailUser() {
+  const mailTransporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: process.env.PORT,
+    secure: true,
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.PASSWORD,
+    },
+  });
 
-//   console.log(mailTransporter);
-//   let mailDetails = {
-//     from: "mainahmwangi12@gmail.com",
-//     to: "mainahmwangi12@gmail.com",
-//     subject: "This is a nodemailer test",
-//     text: "Am testing to see if we can host mail functions in a server and use them in our apps",
-//   };
+  console.log(mailTransporter);
+  let mailDetails = {
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    subject: "This is a nodemailer test",
+    text: "Am testing to see if we can host mail functions in a server and use them in our apps",
+  };
 
-//   mailTransporter.sendMail(mailDetails, (err, data) => {
-//     console.log(data);
-//     if (err) {
-//       console.log(err);
-//     } else {
-//       console.log("Email sent successfully");
-//     }
-//   });
-// }
+  mailTransporter.sendMail(mailDetails, (err, data) => {
+    console.log(data);
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Email sent successfully");
+    }
+  });
+}
 
-// sendEmailUser();
+sendEmailUser();
